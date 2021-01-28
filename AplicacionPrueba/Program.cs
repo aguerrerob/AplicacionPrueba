9b0847c2f78bee97c3881c65005360aba5eabd4a
Alexa <galekxa@gmail.com>
2021-01-27 20:03:51 -0500

    Agregar archivos de proyecto.

---
+++ AplicacionPrueba/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+
+                Console.WriteLine(2 + (10 / 5 * 2) - 7 % 1);
+                Console.ReadKey();
+            }
+        }
+    }
